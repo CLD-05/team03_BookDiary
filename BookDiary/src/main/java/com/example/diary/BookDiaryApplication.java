7d0109e2dffa5c3ce3dd017a3826909cfdd57a6b
chatnip <commcencer@gmail.com>
2026-03-31 13:52:54 +0900

    Project Init

---
+++ BookDiary/src/main/java/com/example/diary/BookDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.example.diary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookDiaryApplication.class, args);
+	}
+
+}

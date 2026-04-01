f5c12d021d71c77d3a757cb3962708c02b7c6630
sihyun <sini5971@gmail.com>
2026-04-01 14:15:39 +0900

    "feat: 서재 등록 API 구현 및 공통 응답 구조 적용
    - POST /api/diary 구현
    - Diary 생성 및 저장 처리
    - BookController ApiResponse 구조로 수정

--- BookDiary/src/main/java/com/example/diary/book/controller/BookController.java
+++ BookDiary/src/main/java/com/example/diary/book/controller/BookController.java
@@ -2,24 +2,31 @@ package com.example.diary.book.controller;
 
 import com.example.diary.book.dto.BookSearchResponse;
 import com.example.diary.book.service.BookService;
+import com.example.diary.common.controller.BaseController;
+import com.example.diary.common.dto.ApiResponse;
+import com.example.diary.common.exception.CustomException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/books")
-public class BookController {
+public class BookController extends BaseController{
 
     private final BookService bookService;
 
     @GetMapping("/search")
-    public List<BookSearchResponse> searchBooks(@RequestParam("keyword") String keyword) {
-        return bookService.searchBooks(keyword);
+    public ResponseEntity<ApiResponse<List<BookSearchResponse>>> searchBooks(
+            @RequestParam("keyword") String keyword
+    ) {
+        if (keyword == null || keyword.trim().isEmpty()) {
+            throw CustomException.badRequest("검색어는 필수입니다.");
+        }
+
+        List<BookSearchResponse> response = bookService.searchBooks(keyword);
+        return ok(response);
     }
 }
